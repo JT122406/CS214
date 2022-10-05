20430c084f775b05395e104f223fdf9010f09fe9
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-10-05 11:51:23 -0400

    Make it compile

--- Project1/sort/sort.c
+++ Project1/sort/sort.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-
-int main(int argc, char *argv[]){    
-    if((argc < 2) || (*argv[1] == '\0')){  //If a command line input is there 
-        char* search = argv[1]; //The string to search for and if we are case sensetive
-        if (argv[1][0] == '-'  && argv[1][1] == 'n'){
-            numeric;
-            return 0;
-        }  
-    }
-
-    char line[100];
-    while (scanf("%s", line) != EOF){
-
-    }
-
-}
 
 int numeric(){  //called when doing a numeric sort
-    int current = 0;
+    int* current = malloc(sizeof(int));
     int* array = malloc(sizeof(int));
+    int i = 0;
     while (scanf("%d", current) != EOF){
-        int i = 0;
-        array[i] = current;
+        array[i] = current[0];
+        current[0] = '\0';
         i++; 
-        array = realloc(array, sizeof(int) * (i + 1));
     }
     for (int i = 0; i < sizeof(array); i++)
     {
@@ -44,3 +29,21 @@ int numeric(){  //called when doing a numeric sort
         printf("%c", '\n');
     }
 }
+
+
+int main(int argc, char *argv[]){    
+    if((argc < 2) || (*argv[1] == '\0')){  //If a command line input is there 
+        char* search = argv[1]; //The string to search for and if we are case sensetive
+        if (argv[1][0] == '-'  && argv[1][1] == 'n'){
+            numeric;
+            return 0;
+        }  
+    }
+
+    char line[100];
+    while (scanf("%s", line) != EOF){
+
+    }
+
+}
+
