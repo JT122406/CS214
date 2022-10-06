3b6d5f98b2354630adb5810be34637e78f517aa6
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-10-06 19:07:33 -0400

    Uniq.c try 2

--- Project1/uniq/uniq2.c
+++ Project1/uniq/uniq2.c
@@ -5,8 +5,34 @@
 
 int main()
 {
-    while (scanf("%s", current) != EOF) {
+    char current [100][100]; //current line
+    int apperiences[100]; //array of apperiences
+    int i = 0;
+    while(scanf("%s", current[i]) != EOF) {
+        if (i == 0)
+        {
+            i++;
+            break;
+        }
 
-    
+        for (int j = 0; j < i; j++)
+        {
+            if (strcmp(current[i], current[j]) == 0)
+            {
+                apperiences[j]++;
+                break;
+            }
+            else if (j == i - 1)
+            {
+                i++;
+                break;
+            }
+        }
+    }
+    for (int j = 0; j < i; j++)
+    {
+        printf("%d ", apperiences[j]);
+        printf("%s", current[j]);
+        printf("%c", '\n'); 
     }
 }
