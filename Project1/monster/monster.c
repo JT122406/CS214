c241bf2c31625b4b89a1ec799e4cb107e44ff4f8
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-10-06 17:26:11 -0400

    Take in Monster command line args

--- Project1/monster/monster.c
+++ Project1/monster/monster.c
@@ -59,6 +59,22 @@ void printMap(int mapX, int mapY, int playerX, int playerY, int goalX, int goalY
 }
 
 int main(int argc, char *argv[]){
+
+    if((argc < 8) || (*argv[1] == '\0')){  //Makes sure there are command line arguments
+        return 0;
+    }
+    int boardX = atoi(argv[1]);
+    int boardY = atoi(argv[2]);
+    int plrX = atoi(argv[3]);
+    int plrY = atoi(argv[4]);
+    int goalX = atoi(argv[5]);
+    int goalY = atoi(argv[6]);
+    int monX = atoi(argv[7]);
+    int monY = atoi(argv[8]);
+
+
+
+
 printMap(4, 6, 0, 0, 2, 4, 1, 1);
 return EXIT_SUCCESS;
 
