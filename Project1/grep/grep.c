923d246e7a558370c5d3821193ef4d0a76a438d6
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-10-06 02:14:20 -0400

    Bro I missed a }

--- Project1/grep/grep.c
+++ Project1/grep/grep.c
@@ -18,58 +18,59 @@ int main(int argc, char *argv[]) {
     int boolean = 0;
     char line[100];
 
-if (caseSensetive == 0)  //case do not matter
-{
-    while (scanf(" %100[0-9a-zA-Z ]", line) != EOF) { //Reads in the string to search
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (tolower(line[i]) == tolower(search[0])){  //First Character Matches the looking string
-            int j = 1;
-            do
-            {
-                i++;
-                    if (tolower(search[j]) != tolower(line[i])){
-                        boolean = 1;
+    if (caseSensetive == 0)  //case do not matter
+    {
+        while (scanf(" %100[0-9a-zA-Z ]", line) != EOF) { //Reads in the string to search
+            for (int i = 0; line[i] != '\0'; i++) {
+                if (tolower(line[i]) == tolower(search[0])){  //First Character Matches the looking string
+                int j = 1;
+                do
+                {
+                    i++;
+                        if (tolower(search[j]) != tolower(line[i])){
+                            boolean = 1;
+                            break;
+                        } else if(tolower(search[0]) == tolower(line[i])){
+                            i--;
+                            break;
+                        }
+                        j++;
+                } while (i < strlen(search) && i < strlen(line));  //prevent possible null pointer
+                    if (boolean == 0) {
+                        printf("%s", line);
+                        printf("%c", '\n');
                         break;
-                    } else if(tolower(search[0]) == tolower(line[i])){
-                        i--;
-                        break;
-                    }
-                    j++;
-            } while (i < strlen(search) && i < strlen(line));  //prevent possible null pointer
-                if (boolean == 0) {
-                    printf("%s", line);
-                    printf("%c", '\n');
-                    break;
-                } 
+                    } 
+                }
             }
+            boolean = 0;
         }
-        boolean = 0;
-    }
-} else { //case sensetive 
-    while (scanf(" %100[0-9a-zA-Z ]", line) != EOF) { //Reads in the string to search
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (line[i] == search[0]){  //First Character Matches the looking string
-            int j = 1;
-            do
-            {
-                i++;
-                    if (search[j] != line[i]){
-                        boolean = 1;
+    } else { //case sensetive 
+        while (scanf(" %100[0-9a-zA-Z ]", line) != EOF) { //Reads in the string to search
+            for (int i = 0; line[i] != '\0'; i++) {
+                if (line[i] == search[0]){  //First Character Matches the looking string
+                int j = 1;
+                do
+                {
+                    i++;
+                        if (search[j] != line[i]){
+                            boolean = 1;
+                            break;
+                        } else if(search[0] == line[i]){
+                            i--;
+                            break;
+                        }
+                        j++;
+                } while (i < strlen(search) && i < strlen(line));  //prevent possible null pointer
+                    if (boolean == 0) {
+                        printf("%s", line);
+                        printf("%c", '\n');
                         break;
-                    } else if(search[0] == line[i]){
-                        i--;
-                        break;
-                    }
-                    j++;
-            } while (i < strlen(search) && i < strlen(line));  //prevent possible null pointer
-                if (boolean == 0) {
-                    printf("%s", line);
-                    printf("%c", '\n');
-                    break;
-                } 
+                    } 
+                }
             }
+            boolean = 0;
         }
-        boolean = 0;
     }
     return 0;
 }
