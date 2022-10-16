21c33ee0684792f9acc0aa94abdb55a5b2c5ab51
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-10-15 22:33:22 -0400

    Take in Command Line args for ls.c

---
+++ hw2/ls.c
@@ -0,0 +1,14 @@
+
+
+
+int main(int argc, char *argv[]){
+    int info;
+    if((argc < 2) || (*argv[1] == '\0')){  //Makes sure there is a string to search for
+        info = 0;
+    } else if (argv[1][0] == '-'  && argv[1][1] == 'l')
+    {
+        info= 1;
+    }
+    
+
+}
