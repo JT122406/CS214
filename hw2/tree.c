dcd3179d848a6b6a9e3d2a26d8bcc93f0f886b62
Christian DaSilva <christiandasilva7663@gmail.com>
2022-10-18 00:09:46 -0400

    tree prints (not sorted)

--- hw2/tree.c
+++ hw2/tree.c
@@ -2,17 +2,48 @@
 #include <dirent.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
-DIR *directory;
-struct dirent *entry;
-directory = opendir(".");
-if(directory == NULL){
-    return 1;
+void tree(char *basePath, const int root);
+
+int main()
+{
+
+
+    tree(".", 0);
+
+    return 0;
 }
-while((entry = readdir(directory)) != NULL){
-    printf("- %s\n", entry->d_name);
-}
-closedir(directory);
-//this is an addition
-return 0;
+
+
+
+void tree(char *basePath, const int root)
+{
+    int i;
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(basePath);
+
+    if (!dir)
+        return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            for (i=0; i<root; i++) 
+            {
+
+                printf("  ");
+
+            }
+
+            printf("- %s\n", dp->d_name);
+
+            strcpy(path, basePath);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+            tree(path, root + 1);
+        }
+    }
+
+    closedir(dir);
 }
