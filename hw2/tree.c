62889e1df6e4fce03dc080a7d089104fcd6b0043
Christian DaSilva <christiandasilva7663@gmail.com>
2022-10-19 22:40:51 -0400

    Tree works, just needs to be sorted

--- hw2/tree.c
+++ hw2/tree.c
@@ -8,12 +8,14 @@ struct Node
 struct Node *next;
 struct Node *root;
 char data[1000];
+//struct dirent *data2;
 int level;
 int isDir;
 
 };
 
-void print_Node(struct Node* head2){
+void print_Node(struct Node* head){
+    struct Node* head2 = head;
     if(head2 == NULL)
     return;
     while(head2 != NULL){
@@ -21,8 +23,8 @@ void print_Node(struct Node* head2){
         printf("  ");
         }
         printf("- %s\n", head2->data);
-        if(head2->isDir)
-        print_Node(head2->root);
+        if(head2->root!=NULL)
+        print_Node(head2->root->next);
         head2 = head2->next;
     }
 }
@@ -31,12 +33,18 @@ if(head->root != NULL){
     free_Node(head->root);
 }
 if(head->next != NULL){
+
     free_Node(head->next);
 }
+//if(head-> data2 != NULL ){
+//    closedir(opendir(head->data2->d_name));
+//}
 free(head);
+
 
 }
 
+//struct Node* 
 void tree(char *basePath, const int root, struct Node* head);
 
 struct Node* create_node(){
@@ -47,14 +55,67 @@ struct Node* create_node(){
     head4->root = NULL;
     head4->isDir = 0;
     head4->level = 0;
+    //head4->data2 = file;
     return head4;
 }
+/*
+void swap(struct Node* node1, struct Node* node2){
+    //struct Node* node1_next = node1->next;
+    //struct Node* node2_next = node2->next;
+    node1->next = node2->next;
+    node2->next = node1;
+}
+
+void bubbleSort(struct Node *start) 
+{ 
+    int swapped; 
+    struct Node *ptr1; 
+    struct Node *lptr = NULL; 
+  
+    Checking for empty list 
+    if (start->next == NULL) 
+        return; 
+  
+    do
+    { 
+        swapped = 0; 
+        ptr1 = start->next; 
+        
+  
+        while (ptr1->next != lptr) 
+        { 
+            if(ptr1->isDir){
+                bubbleSort(ptr1->root);
+            }
+            if (strcasecmp(ptr1->data, ptr1->next->data) > 0) 
+            { 
+                swap(ptr1, ptr1->next); 
+                swapped = 1; 
+            } 
+            ptr1 = ptr1->next; 
+        } 
+        lptr = ptr1; 
+    } 
+    while (swapped); 
+} 
+
+*/
+
+
+
+
+
+
+
+
 
 int main()
 {
 
-struct Node* head = create_node();
-    //tree(".", 0, head);
+//struct Node* head = create_node();
+struct Node* head = create_node(); 
+tree(".", 0, head);
+/*    
 struct Node* child = create_node();
 struct Node* bext = create_node();
 struct Node* child_next = create_node();
@@ -74,43 +135,61 @@ struct Node* next_next = create_node();
     //bext->root = child;
     //bext->isDir = 1;
     //struct Node* test = head;
-    print_Node(head);
+*/
+    //bubbleSort(head);
+    printf(".\n");
+    print_Node(head->next);
     free_Node(head);
-
+    
     return 0;
 }
 
 
 
+//struct Node* 
 void tree(char *basePath, const int root, struct Node* head)
 {
     char path[1000];
     struct dirent *dp;
     DIR *dir = opendir(basePath);
 
-
-    if (!dir)
+    if (!dir){
+        //head->isDir = 0;
         return;
-    struct Node* new_Node = create_node();
-    struct Node* current = create_node();
-    new_Node -> level = root;
-    head->root = new_Node;
-    head->isDir = 1;
+    }
+    //struct Node* new_node = create_node(NULL);
+    struct Node* current = head;
     while ((dp = readdir(dir)) != NULL)
     {
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-
-            strcpy(new_Node->data,dp->d_name);
-            current = new_Node;
+            //printf("%c%c%s\n", 195, 196, dp->d_name);
+            //strcpy(current->data->d_name, dp->d_name);
+            //new_node->data = dp;
+            current->next = create_node();
+            strcpy(current->next->data, dp->d_name);
+            current = current->next;
+            current->level = root;
+            
+            if(dp->d_type == DT_DIR){
+            current->isDir = 1;
+            current->root = create_node();
             strcpy(path, basePath);
             strcat(path, "/");
             strcat(path, dp->d_name);
-            tree(path, root + 1, new_Node);
-            new_Node = current -> next;
+            tree(path,root+1,current->root);
+            }
+            //current ->isDir = 1;
+            //current ->next = new_node;
             
+            //head-> next = tree(path, root + 1, new_node);
+            //return head->next;
+            //head = head -> next;
         }
+        //current = head -> next;
+        //tree(path, root, current->root);
     }
 
     closedir(dir);
+    //return head;
 }
