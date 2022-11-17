e2ba408757bfbf77a4fb89adcf061ed225cc0590
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-11-16 21:59:35 -0500

    Read things in now

--- hw3/shell.c
+++ hw3/shell.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
 
-
+char* readline(){
+    char *line = NULL;
+    size_t len = 0;
+    getline(&line, &len, stdin);
+    return line;
+}
 
 int main(){
-    printf("> ");
+    
+    do{
+        printf("> ");
+        char *line = readline();
+        
+    }while(1);
+    
+    
 }
