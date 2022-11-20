79f3b176ac7dc8b4068d9b77e434f646a2f206a7
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-11-20 14:09:06 -0500

    add a var to hold working dir

--- hw3/shell.c
+++ hw3/shell.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(){
     //int i;
     //char *exit_stuff = "exit\n";
     //int isExit = 1;
     char *line = NULL;
+    char *dir = NULL;
+    getcwd(&dir, 1024);
     do{
         printf("> ");
         size_t len = 0;
         if(getline(&line, &len, stdin) == EOF  || !strcmp("exit\n", line))
             break;
          //very important check strings with a \n get line adds it to the end of a string
+
+        if (strstr(*line, "cd") != NULL)
+        {
+            
+        }
+         
         printf("%s", line);
         free(line);
     }while(1);
     free(line);
+    printf("\n");
     return 0;
     
 }
