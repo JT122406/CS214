76082de764a75c1e455c07fe8d6df76134fc545c
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-12-04 18:50:57 -0500

    add methods todo

---
+++ hw4/mymalloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void myinit(int allocAlg){
+
+}
+
+
+void* mymalloc(size_t size){
+
+}
+
+
+void myfree(void* ptr){
+
+}
+
+
+void* myrealloc(void* ptr, size_t size){
+
+}
+
+
+void mycleanup(){
+
+}
