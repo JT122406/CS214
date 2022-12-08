3e86ac64a09d42d11631f0c7138ee834dab595a5
Christian DaSilva <christiandasilva7663@gmail.com>
2022-12-07 23:53:35 -0500

    mymalloc.h finished

---
+++ hw4/mymalloc.h
@@ -0,0 +1,15 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+void myinit(int allocAlg);
+void* mymalloc(size_t size);
+void myfree(void* ptr);
+void* myrealloc(void* ptr, size_t size);
+void mycleanup();
+
+#endif
